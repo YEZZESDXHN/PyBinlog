1903dc6249f61096d8fdbefe16860d7c5ffa66cc
wzc <1234@valeo.com>
2025-07-17 16:23:07 +0800

    First update

---
+++ BusMirror/busmirror.py
@@ -0,0 +1,36 @@
+from PyBLF.pyBLFLib import *
+
+
+class CanMessage(BlfObjectWrapper):
+    obj: Optional[VBLCANFDMessage64] = None
+
+    def __init__(self):
+        super().__init__(BL_OBJ_TYPE.BL_OBJ_TYPE_CAN_FD_MESSAGE_64, sizeof(VBLCANFDMessage64), VBLCANFDMessage64())
+
+    def filter(self):
+        return self.obj.mChannel == 3
+class ethMessage(BlfObjectWrapper):
+    obj: Optional[VBLEthernetFrame] = None
+
+    def __init__(self):
+        super().__init__(BL_OBJ_TYPE.BL_OBJ_TYPE_ETHERNET_FRAME, sizeof(VBLEthernetFrame), VBLEthernetFrame())
+
+    def filter(self):
+        return True
+
+can_msg = CanMessage()
+eth_msg = ethMessage()
+reader = BlfReader()
+if reader.open("ethlog.blf") is False:
+    print("Open Error!")
+reader.enroll(can_msg)
+reader.enroll(eth_msg)
+while (obj := reader.read_data()) is not None:
+    if obj is can_msg:
+        print('can')
+        print(can_msg.obj.mHeader.mObjectTimeStamp, can_msg.obj.mID, can_msg.obj.mChannel, can_msg.obj.mData)
+    # if obj is eth_msg:
+    #     print('eth')
+    #     print(eth_msg.obj.mHeader.mObjectTimeStamp, eth_msg.obj.mSourceAddress, eth_msg.obj.mDestinationAddress, eth_msg.obj.mType,eth_msg.obj.mPayLoadLength,eth_msg.obj.mPayLoad)
+
+reader.close()
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # PyBinlog
 读写Vector blg格式log数据
+
+源码地址：https://github.com/plumnew/PyBLF
+参考Vector官方demo:https://support.vector.com/sys_attachment.do?sys_id=4abdf7fa871d8e948816dd383cbb3501
+https://support.vector.com/kb?id=kb_article_view&sysparm_article=KB0014829,
